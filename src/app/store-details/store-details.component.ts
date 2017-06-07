96db33e43e7e993a1f2d605de553870b65a0b204
MrCalico <CalicoCat@Hotmail.com>
2017-06-06 22:02:55 -0700

    add buttons store list

--- src/app/store-details/store-details.component.ts
+++ src/app/store-details/store-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import 'rxjs/add/operator/map';
 
@@ -19,18 +19,22 @@ export class StoreDetailsComponent implements OnInit {
   store: IStore = <IStore>{};
   storeKey: string;
 
-  constructor(private db: AngularFireDatabase, private route: Router ) {
+  constructor(private db: AngularFireDatabase, private route: ActivatedRoute ) {
 
-  this.db.list('/stores', {
+  }
+
+  ngOnInit() {
+
+    this.route.params.subscribe((params: Params) => {
+      this.db.list('/stores', {
             query: {
                 orderByChild: 'name',
-                equalTo: this.storeName
+                equalTo: params['id']
               },
               preserveSnapshot: true
             }).subscribe( store => store.map( s => { this.store = s.val(); this.storeKey = s.key; } ) );
-  }
-
-  ngOnInit() { }
+    })
+   }
 
   submit(values) {
 
@@ -41,34 +45,3 @@ export class StoreDetailsComponent implements OnInit {
   }
 
 }
-
-// Expunged Code:
-/*
-  storeForm: FormGroup;
-  store: IStore = {
-    name: 'ralphs',
-    firstName: 'John',
-    lastName: 'Kallie',
-    address: '1010 Wilshire Blvd.',
-    url: 'http://ralphs.com',
-    iconPath: 'c:/temp/icon.jpg'
-  };
-
-      this.store.name = 'Ralphs'
-      this.store.firstName = 'John';
-      this.store.lastName = 'Kallie';
-      this.store.address = '1010 Wilshire Blvd.';
-      this.store.url = 'http://ralphs.com';
-      this.store.iconPath = 'c:/temp/icon.jpg';
-
-      this.storeForm = new FormGroup({
-        name: new FormControl(this.store.name),
-        address: new FormControl(this.store.address),
-        firstName: new FormControl(this.store.firstName),
-        lastName: new FormControl(this.store.lastName)
-      });
-
-    console.log(this.store.name);
-    console.log(this.store);
-    this.storeForm.controls.name.setValue(this.store.name);
-*/
